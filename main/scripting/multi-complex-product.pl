d86e2e433bcd397ba595228dbc2e2059c1801c39
Ankit Pati <contact@ankitpati.in>
2017-08-17 15:14:26 +0530

    product of many complex numbers

---
+++ main/scripting/multi-complex-product.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+
+# Question 8
+# Accept and store an arbitrary number of complex numbers from the user.
+# Use the subroutine from Question 7 to find their product.
+
+use strict;
+use warnings;
+
+sub cmpmul {                                        # function from Question 7
+    my ($a, $b) = @_;
+
+    return {
+        r => $a->{r} * $b->{r} - $a->{i} * $b->{i}, # real
+        i => $a->{r} * $b->{i} + $a->{i} * $b->{r}, # imaginary
+    };
+}
+
+my @cmpls;
+
+print <<"EOF";
+Enter complex numbers to be multiplied.
+    Real & Imaginary parts separated by space,
+    numbers separated by newline,
+    blank line to terminate.
+
+EOF
+
+while (<>) {
+    chomp;
+    last unless $_;
+
+    my @parts = split ' ';
+    push @cmpls, {
+        r => $parts[0],
+        i => $parts[1],
+    };
+}
+
+my $result = {
+    r => 1,
+    i => 0,
+};
+
+$result = cmpmul $result, $_ foreach @cmpls;
+
+print "Result = $result->{r} + $result->{i}i\n";
